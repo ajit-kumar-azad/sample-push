e5a0194194c6ad5e23e3b96274337974dd5676b5
Ajit Kumar <ajit.kumar@walkingtree.in>
2017-02-22 15:28:43 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAA2patDjs:APA91bGoAqzSrEe-ZKKAR2n099tCytdLK7PEVy-IW-4LYuJzIhkrIPpOpTF_1olTBfeTSOyS7RatNA6-5PXk1lCiMl2WZpqWTtDu9d6lb8BaxW_aLPhgc6CjH8-3tKMHNnPqzrqERgVI'
+};
